4b1083b7c2bc4f51bb7fde85844e1db2efcaae24
Gerardo Sánchez <51423594+sandezcode@users.noreply.github.com>
2025-08-18 16:05:36 -0700

    Create 2025_08_18_183644_create_tasks_table.php

---
+++ database/migrations/2025_08_18_183644_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('task_id');
+            $table->string('name', 45);
+            $table->string('description', 255);
+            $table->date('start_date');
+            $table->foreignId('user_id')->constrained('users', 'user_id');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};

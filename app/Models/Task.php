f52805e1e8a973326eb3a095fdcbd690c567c3f3
Gerardo Sánchez <51423594+sandezcode@users.noreply.github.com>
2025-08-18 16:05:45 -0700

    Create Task.php

---
+++ app/Models/Task.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'task_id';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'start_date',
+        'user_id',
+        'status'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+}

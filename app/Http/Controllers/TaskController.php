9e8f21b72c15fecc9b43c50982c871b4162a8059
Gerardo Sánchez <51423594+sandezcode@users.noreply.github.com>
2025-08-19 01:37:19 -0700

    Create TaskController.php

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Obtiene al usuario actualmente autenticado y decodifica los datos JSON de la solicitud.
+        $user = $request->user();
+        $requestData = json_decode($request->getContent(), true);
+
+        if($requestData){
+            /**
+             * Valida los datos entrantes de la solicitud.
+             */
+            $dataValidation = Validator::make($requestData, [
+                'name' => ['required', 'string', 'max:45'],
+                'description' => ['string', 'max:255'],
+                'start_date' => ['required', 'date']
+            ]);
+
+            if($dataValidation->fails()){
+                return response()->json([
+                    'success' => false,
+                    'step' => 1,
+                    'errors' => $dataValidation->errors()->all()
+                ], 400);
+            }
+
+            /**
+             * Verifica que la fecha de inicio de la tarea que se creará no sea menor que la fecha del día actual.
+             */
+            $startDate = Carbon::parse($requestData['start_date']);
+            $currentDate = Carbon::today();
+
+            if($startDate < $currentDate){
+                return response()->json([
+                    'success' => false,
+                    'step' => 2,
+                    'errors' => ['La fecha de inicio no puede ser menor que la fecha actual.']
+                ], 403);
+            }
+
+            /**
+             * Crea la tarea.
+             */
+            $task = Task::create([
+                'name' => $requestData['name'],
+                'description' => $requestData['description'],
+                'start_date' => $requestData['start_date'],
+                'user_id' => $user->user_id,
+                'status' => 0
+            ]);
+
+            /**
+             * Prepara la respuesta:
+             * Añade información adicional al objeto $task.
+             */
+            //------------------------------ Propiedades ocultas ------------------------------
+            $task->user->makeHidden('email_verified_at');
+            $task->makeHidden('user_id');
+
+            /**
+             * Envía una respuesta JSON con todos los detalles al usuario.
+             */
+            return response()->json([
+                'success' => true,
+                'message' => 'La tarea se ha creado.',
+                'data' => [
+                    'task' => $task
+                ]
+            ], 201);
+        }else{
+            return response()->json([
+                'success' => false,
+                'step' => 0,
+                'errors' => ['La solicitud llegó sin datos.']
+            ], 400);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}

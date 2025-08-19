dc2d7a58a2f9b11a7d0ca5267eac9fbb6184da85
Gerardo Sánchez <51423594+sandezcode@users.noreply.github.com>
2025-08-19 14:35:20 -0700

    Update TaskController.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,7 +12,7 @@ class TaskController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): \Illuminate\Http\JsonResponse
     {
         /**
          * Consulta el listado de tareas.
@@ -45,7 +45,7 @@ class TaskController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): \Illuminate\Http\JsonResponse
     {
         // Obtiene al usuario actualmente autenticado y decodifica los datos JSON de la solicitud.
         $user = $request->user();
@@ -133,9 +133,88 @@ class TaskController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id): \Illuminate\Http\JsonResponse
     {
-        //
+        // Obtiene al usuario actualmente autenticado y decodifica los datos JSON de la solicitud.
+        $user = $request->user();
+        $requestData = json_decode($request->getContent(), true);
+
+        if($requestData){
+            /**
+             * Valida los datos entrantes de la solicitud.
+             */
+            $dataValidation = Validator::make($requestData, [
+                'name' => ['required', 'string', 'max:45'],
+                'description' => ['string', 'max:255'],
+                'start_date' => ['required', 'date']
+            ]);
+
+            if($dataValidation->fails()){
+                return response()->json([
+                    'success' => false,
+                    'step' => 1,
+                    'errors' => $dataValidation->errors()->all()
+                ], 400);
+            }
+
+            /**
+             * Verifica que la fecha de inicio de la tarea que se actualizará no sea menor que la fecha del día actual.
+             */
+            $startDate = Carbon::parse($requestData['start_date']);
+            $currentDate = Carbon::today();
+
+            if($startDate < $currentDate){
+                return response()->json([
+                    'success' => false,
+                    'step' => 2,
+                    'errors' => ['La fecha de inicio no puede ser menor que la fecha actual.']
+                ], 403);
+            }
+
+            /**
+             * Consulta y verifica la existencia de la tarea que se actualizará perteneciente al usuario.
+             */
+            $task = Task::where('task_id', $id)->where('user_id', $user->user_id)->first();
+
+            if($task){
+                $task->name = $requestData['name'];
+                $task->description = $requestData['description'];
+                $task->start_date = $requestData['start_date'];
+                $task->save();
+
+                /**
+                 * Prepara la respuesta:
+                 * Añade información adicional al objeto $task.
+                 */
+                $task['start_date_human'] = Carbon::parse($task->start_date)->translatedFormat('d F Y');
+                //------------------------------ Propiedades ocultas ------------------------------
+                $task->user->makeHidden('email_verified_at');
+                $task->makeHidden('user_id');
+
+                /**
+                 * Envía una respuesta JSON con todos los detalles al usuario.
+                 */
+                return response()->json([
+                    'success' => true,
+                    'message' => 'La tarea se ha actualizado.',
+                    'data' => [
+                        'task' => $task
+                    ]
+                ]);
+            }else{
+                return response()->json([
+                    'success' => false,
+                    'step' => 3,
+                    'errors' => ['Tarea no encontrada.']
+                ], 404);
+            }
+        }else{
+            return response()->json([
+                'success' => false,
+                'step' => 0,
+                'errors' => ['La solicitud llegó sin datos.']
+            ], 400);
+        }
     }
 
     /**
